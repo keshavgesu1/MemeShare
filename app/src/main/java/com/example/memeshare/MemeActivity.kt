a221325246a9fb6af2133e9de5f30cdbb1e7ff0b
keshav <keshavraj4546@gmail.com>
2023-04-21 14:00:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/memeshare/MemeActivity.kt
@@ -0,0 +1,31 @@
+package com.example.memeshare
+
+import android.app.DownloadManager.Request
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import java.net.URL
+
+class MemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meme)
+    }
+
+    fun shareMeme(view: View) {
+
+    }
+
+    fun nameMeme(view: View) {
+
+    }
+
+    private fun loadMeme() {
+
+    }
+
+
+}
